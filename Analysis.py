45eaaeaace524e78375a77d84661b68d87c72956
John Paul Lee <johnpaullee@Johns-MacBook-Pro.local>
2020-03-30 19:14:20 +0100

    Upload of initial Analysis.py.

---
+++ Analysis.py
@@ -0,0 +1,13 @@
+#John Paul Lee
+#Analysis of Iris Data Set
+
+#Import Pandas for Data Management 
+import pandas as pd
+
+#Import Numpty and Scipy.stats for analysis
+import numpy as np
+from scipy import stats
+
+#Import the data
+f = pd.read_csv('Iris.Data.csv')
+
